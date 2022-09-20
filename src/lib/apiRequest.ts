322f061e4a13ede45dd77365ae61282dbaa4caca
dnhieu1992 <dnhieu1992@gmail.com>
2022-09-20 11:57:52 +0700

    update base url

--- src/lib/apiRequest.ts
+++ src/lib/apiRequest.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import userService from '../ultils/userService';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://product-collection-api.herokuapp.com/api',
     headers: {
         "Content-Type": "application/json",
     }
